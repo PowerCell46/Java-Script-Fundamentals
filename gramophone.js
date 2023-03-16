0fd6acd60b2409215560181e1241dadaea23e315
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-03-16 23:34:15 +0200

    Delete gramophone.js

--- gramophone.js
+++
@@ -1,6 +0,0 @@
-function gramophone(band, album, song) {
-let songDuration = (album.length * band.length) * song.length / 2;
-let rotations = songDuration / 2.5;
-console.log(`The plate was rotated ${Math.ceil(rotations)} times.`)
-}
-gramophone('Rammstein', 'Sehnsucht', 'Engel');
