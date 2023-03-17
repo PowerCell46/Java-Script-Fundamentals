272e1c0b2af719a216802297f975a3cb98dcd8ce
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-03-17 20:58:05 +0200

    Delete requiredReading.js

--- requiredReading.js
+++
@@ -1,9 +0,0 @@
-function reading(numberOfPages, pagesForOneHour, numberOfDays) {
-let result = numberOfPages / pagesForOneHour;
-result = result / numberOfDays;
-console.log(result);
-}
-reading(432,
-    15 ,
-    4 
-);    
